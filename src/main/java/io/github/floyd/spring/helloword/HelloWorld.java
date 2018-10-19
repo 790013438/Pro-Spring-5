99775ed954d0ffa47878e847a107df49d647f41d
790013438 <790013438@qq.com>
2018-10-19 17:10:32 +0800

    从控制台传入参数

--- src/main/java/io/github/floyd/spring/helloword/HelloWorld.java
+++ src/main/java/io/github/floyd/spring/helloword/HelloWorld.java
@@ -1,8 +1,20 @@
 package io.github.floyd.spring.helloword;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author floyd
+ */
 public class HelloWorld {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
     public static void main(String... args) {
-        System.out.println("Hello World!");
+        if (args.length > 0) {
+            logger.debug(args[0]);
+        } else {
+            logger.debug("Hello World!");
+        }
     }
 }
