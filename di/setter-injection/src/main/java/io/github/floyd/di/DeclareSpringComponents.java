ac6fa3a93510a6f432d09befa090114c0f613d53
790013438 <790013438@qq.com>
2020-02-02 19:10:28 +0800

    获取

---
+++ di/setter-injection/src/main/java/io/github/floyd/di/DeclareSpringComponents.java
@@ -0,0 +1,23 @@
+package io.github.floyd.di;
+
+import io.github.floyd.spring.helloworld.decoupled.MessageRenderer;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+/**
+ * @author 程淼
+ * @version 1.0
+ * ClassName io.github.floyd.di
+ * Description: 描述
+ * Company: com.yinhai
+ * date 2018/11/21
+ */
+public class DeclareSpringComponents {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext genericXmlApplicationContext = new GenericXmlApplicationContext();
+        genericXmlApplicationContext.load("classpath:spring/app-context-xml.xml");
+        genericXmlApplicationContext.refresh();
+        MessageRenderer messageRenderer = genericXmlApplicationContext.getBean("renderer", MessageRenderer.class);
+        messageRenderer.render();
+        genericXmlApplicationContext.close();
+    }
+}
